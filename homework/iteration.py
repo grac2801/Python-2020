08eb73f3d26644857564f1a96016d662fcc30f1c
Oscar Gracias <ogracias@SUHSD.NET>
2020-12-06 22:52:12 -0800

    gh3 committing file > > Testing whether this works properly. > I will delete the file later.

---
+++ homework/iteration.py
@@ -0,0 +1,19 @@
+
+#Date: Dec 6, 2020
+#ogracias
+#Current Module: homework.iteration
+#AP Computer Science Principles
+
+#imports, variables, CONSTANTS
+
+
+
+
+#functions
+
+
+
+if __name__ == "__main__":
+    myStr = 'Hello'
+    for i in myStr:
+        print(i)
