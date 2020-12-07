78473668e2c9a7f40f0917a8f5d0e885e03487f9
grac2801 <oscar.gracias@icloud.com>
2020-12-07 11:50:13 -0800

    Second part of lists
    Added the second set of lists and the
    built-in functions. Also how to make a
    new list

--- chapter8/listsP3.py
+++ chapter8/listsP3.py
@@ -10,6 +10,14 @@
 
 
 #functions
+def removeLastItem(L):
+    '''
+    (list) --> list
+    Removes last item in the list
+    [1,2,3,4] --> [1,2,3]
+    '''
+    del L[-1]
+    return L
 
 
 
@@ -91,35 +99,80 @@ if __name__ == "__main__":
     print(marxes)
     
     print()
-    print('----- find the index -----')
+    print('\n\n----- find the index -----')
     print(marxes.index('Raphael'))
     is_here = 'Rachel' in marxes
     print('Rachel found in marxes: ', is_here)
+    print('marxes:', marxes)
+    
+    
+    print('\n\n----- join() -----')
+    separator = ', '
+    print(separator.join(marxes))
+    
+    print('\n\n----- sorted() -----')
+    #Make a copy of your list
+    sortedMarxes = sorted(marxes)
+    print(sortedMarxes)
+    marxes[0] = 'original'
+    print('marxes:', marxes)
+    print('sortedMarxes:', sortedMarxes)
     
     
     
     
+    print('\n\n----- sort -----')
+    print(marxes)
+    marxes.pop(0)
+    print(marxes)
+    
+    #sorting to original list
+    marxes.sort(reverse=True)
+    print('using sort:', marxes)
+    
+    numbers = [2, 1, 6.7, 34, 0.1]
+    numbers.sort(reverse=False)
+    print(numbers)
     
     
     
+    print('\n\n----- Make a function call using a list -----')
+    print(removeLastItem(numbers))
     
     
+    print('\n\n----- Make a new list and aliases -----')
+    a = [1, 2, 3]
+    print('a:', a)
+    b = a[:] #Creating a new list independent of original one
+    print('b:', b)
+    print('id for a', id(a))
+    print('id for b', id(b))
+    a[0] = 1000
+    print('a:', a)
+    print('b:', b)
     
+    c = list()
+    print(c)
+    c.append(123)
+    print(c)
+    d = list()
+    d += [12, 24, 36]
+    e = []
+    e.extend(d)
+    print(d)
+    print(e)
     
+    print('\n\n----- built-ins -----')
+    randomNumbers = [34, 23, 67, 89, 12]
+    emptylist = []
+    print(max(randomNumbers))
+    print(min(randomNumbers))
+    print(sum(randomNumbers))
+    print(any(emptylist))
+    print(chr(177))
+    print(len(randomNumbers))
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    print('updated')
     
     
     
